d59a1f1813d966f3f89b432e03647f6185e19870
kimjaewan1 <yoyoyou@nate.com>
2021-03-30 15:55:43 +0900

    if else

---
+++ src/p04/lecture/A01IF.java
@@ -0,0 +1,16 @@
+package p04.lecture;
+
+public class A01IF {
+	public static void main(String[] args) {
+		// if : 조건의 true일 때만 실행
+		
+		System.out.println("실행");
+		
+		if (true) {
+			System.out.println("실행되거나 안되거나...");
+		}
+		
+		
+		System.out.println("실행2");
+	}
+}
