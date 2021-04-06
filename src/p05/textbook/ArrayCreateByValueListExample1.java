0282fd492e13f8356f75c6b45b32cda685ecc065
kimjaewan1 <yoyoyou@nate.com>
2021-04-06 20:11:30 +0900

    exercise 07

--- src/p05/textbook/ArrayCreateByValueListExample1.java
+++ src/p05/textbook/ArrayCreateByValueListExample1.java
@@ -12,7 +12,7 @@ public class ArrayCreateByValueListExample1 {
 		for (int i = 0; i < 3; i++) {
 			sum += scores[i];
 		}
-		
+		//
 		System.out.println("총합 : " + sum);
 		double avg = (double) sum / 3;
 		System.out.println("평균 : " + avg);
