27d7e459a2275dc7fbf9706421bb5f16333a26bf
kimjaewan1 <yoyoyou@nate.com>
2021-04-06 14:50:23 +0900

    default constructor

---
+++ src/p06/lecture/textbook/s060702/Car.java
@@ -0,0 +1,8 @@
+package p06.lecture.textbook.s060702;
+
+public class Car {
+	//생성자
+	Car(String color, int cc) {
+		
+	}
+}
---
+++ src/p06/lecture/textbook/s060702/CarExample.java
@@ -0,0 +1,6 @@
+package p06.lecture.textbook.s060702;
+
+public class CarExample {
+	Car myCar = new Car("검정", 3000);
+	//Car myCar = new Car(); 
+}
