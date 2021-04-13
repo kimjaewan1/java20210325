4a84d5f1d518908519050851dc1d6c222e5e9f84
kimjaewan1 <yoyoyou@nate.com>
2021-04-13 21:50:33 +0900

    protected

---
+++ src/p07/lecture/p4access_modifer_protected/A01AccessModifierProtected.java
@@ -0,0 +1,5 @@
+package p07.lecture.p4access_modifer_protected;
+
+public class A01AccessModifierProtected {
+
+}
---
+++ src/p07/lecture/p4access_modifer_protected/package2/Child.java
@@ -0,0 +1,5 @@
+package p07.lecture.p4access_modifer_protected.package2;
+
+public class Child {
+
+}
