f3c313523c6be05d31d81e099f56391e47d73259
kimjaewan1 <yoyoyou@nate.com>
2021-04-22 05:27:04 +0900

    enum type

---
+++ src/p05/lecture/enumType/A02EnumType.java
@@ -0,0 +1,37 @@
+package p05.lecture.enumType;
+
+public class A02EnumType {
+	public static void main(String[] args) {
+		String s = "VALUE1";
+
+		switch (s) {
+		case "VAULE1":
+			System.out.println("Value1 입니다.");
+			break;
+		case "VAULE2":
+			System.out.println("Value2 입니다.");
+			break;
+
+		default:
+			System.out.println("둘 다 아닙니다.");
+			break;
+		}
+		
+		System.out.println("enum type");
+		MyEnum e1 = MyEnum.VALUE1;
+		
+		switch (e1) {
+		case VALUE1:
+			System.out.println("value1 입니다.");
+			break;
+		case VALUE2:
+			System.out.println("value2 입니다.");
+			break;
+			
+		default:
+			System.out.println("둘 다 아닙니다");
+			break;
+		}
+	}
+}
+
---
+++ src/p05/textbook/s050701/Week.java
@@ -0,0 +1,11 @@
+package p05.textbook.s050701;
+
+public enum Week {
+	MONDAY,
+	TUESDAU,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
